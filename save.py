bcf609ca8420613ad0a469e1d10c3e485dc6fbda
stephane <stephane@mint-cinamon-stephane>
2026-06-08 19:54:34 +0200

    Sauvegarde

--- save.py
+++ save.py
@@ -3,25 +3,20 @@ import git
 
 REPO = git.Repo(Path(__file__).resolve().parent)
 
-def git_status():
+fichiers_modifies = REPO.index.diff(None)
+fichiers_non_suivis = REPO.untracked_files
 
-	fichiers_modifies = REPO.index.diff(None)
-	fichiers_non_suivis = REPO.untracked_files
+if fichiers_modifies:
+	print("Modifications :")
+	for file in fichiers_modifies:
+		print(f"\t- {file.a_path}")
 
-	if fichiers_modifies:
-		print("Modifications :")
-		for file in fichiers_modifies:
-			print(f"\t- {file.a_path}")
+if fichiers_non_suivis:
+	print("\nNouveaux fichiers :")
+	for file in fichiers_non_suivis:
+		print(f"\t- {file}")
 
-	if fichiers_non_suivis:
-		print("\nNouveaux fichiers :")
-		for file in fichiers_non_suivis:
-			print(f"\t- {file}")
-
-	return fichiers_modifies or fichiers_non_suivis
-
-
-if not git_status():
+if not fichiers_modifies and not fichiers_non_suivis:
 	print("Aucun changement à sauvegarder.")
 
 else:
