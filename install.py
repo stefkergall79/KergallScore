7430f550670131d2cc39f8b258663988a20f6dc9
stephane <stephane@mint-cinamon-stephane>
2026-06-09 19:29:53 +0200

    Modifications

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import os
 os.system("sudo apt update && sudo apt install pip python3-tk -y")
-os.system("pip install gitpython --break-system-packages")
+os.system("pip install gitpython customtkinter --break-system-packages")
 import git
 git_config = git.Git().config
 git_config("user.name", "Stéphane Kergall")
