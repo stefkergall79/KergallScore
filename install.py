6024cc2bba07473845388a9db1c8088ba7e4a626
stephane <stephane@mint-cinamon-stephane>
2026-06-09 19:28:13 +0200

    Modifications

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
-from os import system
-system("sudo apt update && sudo apt install pip")
-system("pip install gitpython --break-system-packages")
+import os
+os.system("sudo apt update && sudo apt install pip python3-tk -y")
+os.system("pip install gitpython --break-system-packages")
 import git
 git_config = git.Git().config
 git_config("user.name", "Stéphane Kergall")
