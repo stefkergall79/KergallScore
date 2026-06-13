19195bcc6244c164e61e260cc250de3c9766e706
Stéphane Kergall <stef.kergall@gmail.com>
2026-06-13 08:28:08 +0200

    Modifications

--- install.py
+++ install.py
@@ -1,23 +1,8 @@
 import os
-
-apt = {
-    "pip": "Installateur de modules Python Pip",
-    "python3-tk": "Module d'interface graphique Tkinter"
-}
-first_package_apt = True
-
-
-pip = {
-    "gitpython": "Module d'interface Git pour Python",
-    "customtkinter": "Module de relooking de l'interface graphique CustomTkinter"
-}
-
-for package in apt:
-    pass
-
 os.system("sudo apt update && sudo apt install pip python3-tk -y")
 os.system("pip install gitpython customtkinter --break-system-packages")
 import git
 git_config = git.Git().config
 git_config("user.name", "Stéphane Kergall")
 git_config("user.email", "stef.kergall@gmail.com")
+git_config("pull.rebase", "false")
