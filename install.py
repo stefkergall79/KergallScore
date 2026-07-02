6dec60e209b3cf1cbd39a463894b325f392a5dde
Stéphane Kergall <stef.kergall@gmail.com>
2026-07-02 19:16:08 +0200

    prise en charge de latex

--- install.py
+++ install.py
@@ -37,7 +37,8 @@ if "--code" in sys.argv:
         "apps": {
             "Extension GABC": "AISCGre-BR.vscode-gregorio",
             "Extension Lilypond": "lhl2617.VSLilyPond",
-            "Extension Python": "ms-python.python"
+            "Extension Python": "ms-python.python",
+            "Extension LaTeX": "James-Yu.latex-workshop"
         }
     }
 
