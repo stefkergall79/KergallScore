66bc336cae05007750cefa32b526633f8a229fce
Stéphane Kergall <stef.kergall@gmail.com>
2026-07-06 12:51:09 +0200

    Modifications

--- .scripts/installer.sh
+++ .scripts/installer.sh
@@ -2,6 +2,7 @@
 set -e
 
 start=$(date +%s.%N)
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")"
 
 apt_list=(
     "timidity"
@@ -18,21 +19,33 @@ code_list=(
     "ms-python.python"
 )
 
+#apt
 sudo apt update
 sudo apt install -y "${apt_list[@]}"
 echo
+
+#pip
 pip install "${pip_list[@]}" --break-system-packages
 echo
+
+#code
 if [[ "$1" == "--code" ]]; then
     code --install-extension "${code_list[@]}"
     echo
 fi
 
+#lilypond web
+curl -L -O "https://gitlab.com/lilypond/lilypond/-/releases/v2.26.0/downloads/lilypond-2.26.0-linux-x86_64.tar.gz"
+sudo tar -xzf lilypond-2.26.0-linux-x86_64.tar.gz -C /opt/
+cp "${HERE}/settings.ily" /opt/lilypond-2.26.0/share/lilypond/2.26.0/ly/
+
+#Git
 echo "Configuration de Git..."
 git config user.name "Stéphane Kergall"
 git config user.email "stef.kergall@gmail.com"
 git config pull.rebase false
 
+#Fin
 end=$(date +%s.%N)
 elapsed=$(awk "BEGIN {print $end - $start}")
 printf "Installation terminée en %.2f secondes.\n" "$elapsed"
--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 > Une base de données. Des commandes. De la musique.
 
 - Vous voudriez relooker une vieille partition de musique multi-photocopiée, bricolée sur 4 pages ?
-- Vous aimeriez un livret propre pour une cérémonie, tout inclus, avec table des matières ?
+- Vous aimeriez un livret complet lors d'une cérémonie pour votre chorale, tout inclus, avec table des matières ?
 - Vous cherchez un éditeur à votre dernière composition ?
 - Vous aimeriez harmoniser une mélodie ?
 
-Je propose des solutions. Contactez-moi : stef.kergall@gmail.com.
+Je propose des solutions. [Envoyez-moi un mail](mailto:stef.kergall@gmail.com) si vous êtes intéressés.
+
+# Une base de données
+
+|     |     |
+| --- | --- |
+|     |     |
